f8d53130213e6cdbf38d59b7eb3acf60952037dd
samikannup53 <samikannup53@gmail.com>
2025-05-03 15:58:38 +0530

    Project Setup and Installed Necessary Dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// Configurations
+
+// Middlewares
+
+// Routes
+
+// Server
+
+app.listen(port, () => {
+  console.log(
+    `App Started at Port:${port} and the URL is : http://localhost:${port}`
+  );
+});
